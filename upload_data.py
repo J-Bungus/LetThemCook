7b8e71f7ab8c72cc46dc398272421b4aa1788a20
Justin Baiyu Wang <justin.wang1ab@gmail.com>
2023-06-17 07:33:33 -0400

    Scraper
    * Scrape the NYT cooking website for recipes
    * Scrape Ethan Chlebowski's website for recipes
    * Upload the scraped data to a MongoDB database
    * Add a script for generating an ingredients list
    
    Next Steps:
    * Process the ingredient data to extract keywords

---
+++ upload_data.py
@@ -0,0 +1,27 @@
+import pymongo
+from pymongo import MongoClient
+import csv
+
+from nyt_scraper import nyt_scraper_wrapper
+from ethan_scraper import ec_scraper_wrapper
+
+uri = "mongodb+srv://LetThemCook:dxTjvlN93vMeJ5tE@letthemcookdb.uok4zr6.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+db = client["LetThemCook"]
+collection = db["Recipes"]
+header = ["RecipeId", "RecipeName", "Source", "TimeRequired", "Ingredients", "Steps"]
+
+with open('./recipes.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    f.close()
+
+recipes = nyt_scraper_wrapper()
+recipes.extend(ec_scraper_wrapper)
+
+recipes = map(lambda x: x.to_dict(), recipes)
+
+collection.insert_many(recipes)
