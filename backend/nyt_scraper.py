68459b4b1e76b6c50c912964f9b0949707228888
liuwllm <liu.wllm@gmail.com>
2023-06-17 14:37:35 -0400

    Merge backend+frontend

---
+++ backend/nyt_scraper.py
@@ -0,0 +1,91 @@
+"""
+Scrapes the New York Times Cooking website
+
+I have no idea how this website is structured but there are random
+numbers in the url for recipe pages and they range from 9 to 12973
+and 1012377 to 1022498. There are randomly missing numbers within those
+ranges so I have no idea whats going on, but this program scrapes
+as much as possible.
+"""
+
+from bs4 import BeautifulSoup as bs
+import requests as req
+from tqdm import tqdm
+
+from class_definition import RecipeData, id
+
+BASE_URL = 'https://cooking.nytimes.com/recipes/'
+
+# generate_urls() returns a list of possible urls for recipes by nyt
+def generate_urls():
+    url_list = []
+
+    for i in range(9, 12974):
+        url_list.append(BASE_URL + str(i))
+
+    """
+    for i in range (1012377, 1022498):
+        url_list.append(BASE_URL + str(i))
+    """
+
+    return url_list
+
+# scrape_recipes(url_list) returns a list of RecipeData
+def scrape_recipes(url_list):
+    global id
+
+    recipes_data= []
+
+    with open('./recipes.csv', 'a') as f:
+        for url in tqdm(url_list):
+            #print(f"Attempting to Scrape: {url}")
+
+            try:
+                r = req.get(url)
+            except ConnectionError:
+                print(f"No recipe from {url}")
+                continue
+            soup = bs(r.text, 'html.parser')
+
+            instruction_element = soup.find_all('p', class_='pantry--body-long')
+            try :
+                time = soup.find('dd', class_='pantry--ui').get_text()
+            except AttributeError:
+                print(f"No recipe from: {url}")
+                continue
+
+            instructions = []
+
+            for element in instruction_element:
+                instructions.append(element.get_text())
+                
+            quantity_element = soup.find('span', class_='ingredient_quantity__wlL75')
+            ingredients = []
+
+            while quantity_element:
+                ingredient_element = quantity_element.find_next_sibling('span')
+                ingredients.append(quantity_element.get_text() + ' ' + ingredient_element.get_text())
+
+                quantity_element = quantity_element.find_next('span', class_='ingredient_quantity__wlL75')
+
+            try:
+                recipe_name = soup.find('h1', class_='pantry--title-display').get_text()
+            except AttributeError:
+                print(f"No recipe from: {url}")
+                continue
+
+            recipe = RecipeData(id, recipe_name, ingredients, instructions, url, time)
+        
+            recipe.write_file(f)
+            recipes_data.append(recipe)
+            id += 1
+
+        f.close()
+
+    return recipes_data
+
+def nyt_scraper_wrapper():
+    recipes = scrape_recipes(generate_urls())
+
+    print("The New York Times scrape has completed!")
+    return recipes
